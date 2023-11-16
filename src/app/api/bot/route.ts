4a2f5a59eaf107193a06747b486cd165de9bc03e
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-11-16 22:19:51 +0000

    added public key check

--- src/app/api/bot/route.ts
+++ src/app/api/bot/route.ts
@@ -1,12 +1,34 @@
-import { type NextRequest, NextResponse } from "next/server";
+import { verifyKey } from "discord-interactions";
+import { NextResponse, type NextRequest } from "next/server";
+import { env } from "~/env.mjs";
+import { commandHandler } from "~/server/bot/command";
 import { interactionSchema } from "~/utils/bot";
 
 export async function POST(req: NextRequest) {
-  const message = interactionSchema.parse(req.body);
+  const rawBody = (await req.body!.getReader().read()).value!;
+
+  const isValidRequest = verifyKey(
+    rawBody,
+    req.headers.get("x-signature-ed25519")!,
+    req.headers.get("x-signature-timestamp")!,
+    env.DISCORD_PUBLIC_KEY,
+  );
+
+  if (!isValidRequest) {
+    return NextResponse.json({ error: "Bad request signature " });
+  }
+
+  const message = interactionSchema.parse(await req.json());
 
   if (message.type === "ping") {
     return NextResponse.json({ type: 1 });
   } else if (message.type === "command") {
+    return NextResponse.json({
+      type: 4,
+      data: {
+        content: await commandHandler.handler(message),
+      },
+    });
   } else {
     console.error("Unknown Type");
     return NextResponse.json({ error: "Unknown Type" });
--- src/server/bot/command.ts
+++ src/server/bot/command.ts
@@ -1,14 +1,9 @@
 import display from "./commands/display";
 import ioweu from "./commands/ioweu";
 import poke from "./commands/poke";
-import settleup from "./commands/settleup";
 import uoweme from "./commands/uoweme";
 import registerCommands from "./lib/register";
 
-export const commands = registerCommands([
-  display,
-  ioweu,
-  poke,
-  settleup,
-  uoweme,
-]);
+export const commands = [display, ioweu, poke, uoweme];
+
+export const commandHandler = registerCommands(...commands);
